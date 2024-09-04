d258e5dce6f96d85238ac48e3a8a753c020ec0a1
Arijit655 <arijitbarik655@gmail.com>
2024-09-04 09:57:11 +0530

    update url

--- src/backendUrl.js
+++ src/backendUrl.js
@@ -1,4 +1,4 @@
-// const url = "https://finance-tracker-backend-8az1.onrender.com"
-const url = "http://localhost:5000"
+const url = "https://finance-tracker-backend-8az1.onrender.com"
+// const url = "http://localhost:5000"
 
 export default url
